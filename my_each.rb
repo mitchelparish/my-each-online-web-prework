58f7d2e314704725bcaebf532013e7369e5fd8ec
Mitchel Parish <mitchelparish@gmail.com>
2018-12-31 22:46:41 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,14 @@
+
 collection = [1, 2, 3, 4]
 
 def my_each(collection)
-  i = 0
-  while i > collection.length
-    yield(collection[i])
-    i += 1
-  end
-  collection
-end # put argument(s) here
-  # code here
+
+ i=0
+ while i < collection.length
+ yield(collection[i])
+ i+=1
+ end
+ collection
+end
+
 my_each(collection) {|x| puts x}
